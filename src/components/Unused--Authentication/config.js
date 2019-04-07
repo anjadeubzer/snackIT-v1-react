194b77e39d59d5a622caea6f127835e27722dfd5
Anja Deubzer <info@anjadeubzer.com>
2019-04-08 01:46:26 +0200

    We don't need this any more - handled in Login Page.

--- src/components/Unused--Authentication/config.js
+++
@@ -1,9 +0,0 @@
-// Main Container IDs
-export const LoginPage = "LoginPage";
-export const siteName = "site-title";
-export const siteDescription = "site-description";
-
-// UI Component IDs
-export const backBtn = "back-to-post";
-export const loginBtn = "login-button";
-export const logoutBtn = "logout-button";
--- src/components/Unused--Authentication/index.js
+++
@@ -1,5 +0,0 @@
-import { init as Authenticate } from "./components/Authentication";
-
-(function init() {
-  Authenticate();
-})();
